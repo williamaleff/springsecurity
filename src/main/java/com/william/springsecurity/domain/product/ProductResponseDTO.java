d5e48446c3001c044de1baea8925159b97cf76ed
WilliamAlefe <totvs.william.alefe@gmail.com>
2025-03-17 11:19:52 -0300

    limpeza do codigo

--- src/main/java/com/william/springsecurity/domain/product/ProductResponseDTO.java
+++
@@ -1,7 +0,0 @@
-package com.william.springsecurity.domain.product;
-
-public record ProductResponseDTO(String id, String name, Integer price) {
-    public ProductResponseDTO(Product product){
-        this(product.getId(), product.getName(), product.getPrice());
-    }
-}
--- src/main/java/com/william/springsecurity/repositories/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.william.springsecurity.repositories;
-
-import com.william.springsecurity.domain.product.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, String> {
-}
